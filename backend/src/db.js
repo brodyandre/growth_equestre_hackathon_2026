1265e607870ef57b9f7e7b5c8352bdba2203fabf
brodyandre <landresouza36@gmail.com>
2026-02-09 20:21:22 -0300

    Revert "Merge pull request #1 from brodyandre/feature/be"
    This reverts commit 042d36370bf75ad445f4c8e5b8765751f79c74d0, reversing
    changes made to d29ecf9e7b0942e301a81a0b5bc3e1fd0be41fab.

--- backend/src/db.js
+++
@@ -1,3 +0,0 @@
-import pg from "pg";
-export const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-export async function query(text, params) { return pool.query(text, params); }
